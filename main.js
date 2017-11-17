d4c7bb4c82c7f2996db728a137727e79d0b491a0
Zack <zseliger@gmail.com>
2017-11-17 17:17:32 -0600

    Week 2

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-window.onload = function() {
- var canvas = document.getElementById("canvas");
- var ctx = canvas.getContext("2d");
+var x = 10;
+var y = 10;
 
- ctx.fillStyle = "#000";
- ctx.fillRect(10, 10, 20, 20);
+window.onload = function() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	
+	document.addEventListener("keydown", keydown);
+	setInterval(main, 1/60 * 1000);
+}
+
+function main() {
+	//render
+	ctx.fillStyle = "#FFF";
+	ctx.fillRect(0,0,100,100);
+	ctx.fillStyle = "#000";
+	ctx.fillRect(x, y, 20, 20);
+}
+
+function keydown(e) {
+	switch(e.keyCode) {
+		case 37:
+			x -= 1;
+			break;
+		case 38:
+			y -= 1;
+			break;
+		case 39:
+			x += 1;
+			break;
+		case 40:
+			y += 1;
+			break;
+	}
 }
