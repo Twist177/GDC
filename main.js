57a2755208663026a8a4138486b47f6c19f2c0a9
Zack <zseliger@gmail.com>
2017-12-01 17:01:41 -0600

    Week 3

--- main.js
+++ main.js
@@ -1,35 +1,89 @@
-var x = 10;
-var y = 10;
+class Rectangle {
+	constructor(x, y, w, h) {
+		this.x = x;
+		this.y = y;
+		this.width = w;
+		this.height = h;
+		this.color = "#000";
+		
+		this.left = false;
+		this.right = false;
+		this.up = false;
+		this.down = false;
+	}
+	update() {
+		if (this.left == true) {
+			this.x -= 1;
+		}
+		if (this.right == true) {
+			this.x += 1;
+		}
+		if (this.up == true) {
+			this.y -= 1;
+		}
+		if (this.down == true) {
+			this.y += 1;
+		}
+	}
+	render() {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+var player = new Rectangle(10, 10, 20, 20);
 
 window.onload = function() {
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	
 	document.addEventListener("keydown", keydown);
+	document.addEventListener("keyup", keyup);
+	
 	setInterval(main, 1/60 * 1000);
 }
 
 function main() {
-	//render
+	//update
+	player.update();
+	
+	//clear screen
 	ctx.fillStyle = "#FFF";
 	ctx.fillRect(0,0,100,100);
-	ctx.fillStyle = "#000";
-	ctx.fillRect(x, y, 20, 20);
+	//render
+	player.render();
 }
 
 function keydown(e) {
 	switch(e.keyCode) {
 		case 37:
-			x -= 1;
+			player.left = true;
 			break;
 		case 38:
-			y -= 1;
+			player.up = true;
 			break;
 		case 39:
-			x += 1;
+			player.right = true;
 			break;
 		case 40:
-			y += 1;
+			player.down = true;
+			break;
+	}
+}
+
+function keyup(e) {
+	switch(e.keyCode) {
+		case 37:
+			player.left = false;
+			break;
+		case 38:
+			player.up = false;
+			break;
+		case 39:
+			player.right = false;
+			break;
+		case 40:
+			player.down = false;
 			break;
 	}
 }
