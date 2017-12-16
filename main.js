7fe46c48691e3cf47da8ea9d8b12a487fa6b7a9e
Zack <zseliger@gmail.com>
2017-12-15 22:10:27 -0600

    Week 5

--- main.js
+++ main.js
@@ -38,6 +38,12 @@ class Rectangle {
 				this.y = prevy;
 			}
 		}
+		
+		for (var i = 0; i < coinArray.length; i++) {
+			if (coinArray[i] != this && checkCollision(this, coinArray[i])) {
+				coinArray.splice(i, 1);
+			}
+		}
 	}
 	render() {
 		ctx.fillStyle = this.color;
@@ -45,15 +51,32 @@ class Rectangle {
 	}
 }
 
+var coinImage = new Image();
+coinImage.src = "coin.png";
+
+class Coin extends Rectangle {
+	constructor(x, y, w, h) {
+		super(x, y, w, h);
+		this.image = coinImage;
+	}
+	render() {
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+	}
+}
+
+//adding things
 var player = new Rectangle(10, 10, 20, 20);
 var rect1 = new Rectangle(40, 40, 20, 10);
 var rectArray = [];
+var coinArray = [];
 rectArray.push(player);
 rectArray.push(rect1);
-rectArray.push(new Rectangle(50,60,10,10));
+coinArray.push(new Coin(50,60,100,100));
 
 window.onload = function() {
 	canvas = document.getElementById("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	ctx = canvas.getContext("2d");
 	
 	document.addEventListener("keydown", keydown);
@@ -65,12 +88,16 @@ window.onload = function() {
 function main() {
 	//clear screen
 	ctx.fillStyle = "#FFF";
-	ctx.fillRect(0,0,100,100);
+	ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
 	
 	//update and render
 	for (var i = 0; i < rectArray.length; i++) {
 		rectArray[i].update();
 		rectArray[i].render();
+	}
+	for (var i = 0; i < coinArray.length; i++) {
+		coinArray[i].update();
+		coinArray[i].render();
 	}
 }
 
@@ -114,3 +141,16 @@ function checkCollision(rect1, rect2) {
 			rect1.y < rect2.y + rect2.height &&
 			rect1.height + rect1.y > rect2.y);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
